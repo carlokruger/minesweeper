89cf92539c48c30429161a516b8e51ecb57aec72
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-11-05 15:36:53 +1300

    completed stage 2

--- App.js
+++ App.js
@@ -1,28 +1,88 @@
-import logo from './bomb.svg';
+import bomb from './bomb.svg';
 import './App.css';
 import React from "react";
 
-function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p class="textLoading">
-            Minesweeper is loading...
-          </p>
-          {/*
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+function Cell(props) {
+    return (
+        <div className="cell">
 
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-*/}
-        </header>
-      </div>
-  );
+        </div>
+    );
+}
+
+function Row(props) {
+    const cells = [0, 1, 2, 3, 4, 5, 6, 7].map(num => <Cell value={num + props.number * 8}/>);
+    return (
+        <div className="row">
+            {cells}
+        </div>
+    );
+}
+
+function Board(props) {
+    const rows = [0, 1, 2, 3, 4, 5, 6, 7, 8].map(num => <Row number={num} />);
+    return (
+        <div className="board">
+            {rows}
+        </div>
+    );
+}
+
+function Counter(props) {
+    return (
+        <div className="counter">
+            💣 10
+        </div>
+    );
+}
+
+function Reset(props) {
+    return(
+        <button className="reset">😄</button>
+    );
+}
+
+function Timer(props) {
+    return (
+        <div className="timer">0:00</div>
+    );
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderBoard() {
+        return (
+            <Board />
+        );
+    }
+
+    renderControl() {
+        return (
+            <div className="control">
+                <Counter />
+                <Reset />
+                <Timer />
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className="Header">
+                        <h2>Minesweeper
+                        <img src={bomb} className="App-logo" alt="logo" /></h2>
+                    </div>
+                    {this.renderControl()}
+                    {this.renderBoard()}
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
