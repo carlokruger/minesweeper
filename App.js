eff9437453177f8f323dc6adfc7c8e7bd71aaaaa
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-12-28 15:20:07 +1300

    moved state init to App

--- App.js
+++ App.js
@@ -7,6 +7,41 @@ import Board from "./components/Board";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      cells: {},
+    };
+  }
+
+  initState = () => {
+    let cells = { ...this.state.cells };
+    for (let cell = 0; cell < 72; cell++) {
+      cells[cell] = {
+        index: cell,
+        isMined: false,
+        isFlagged: false,
+        isOpen: false,
+      };
+    }
+    let mines;
+    for (mines = 0; mines < 11; mines++) {
+      let mined = Math.floor(Math.random() * Math.floor(72));
+      console.log("finding a mine");
+      console.log(mined);
+
+      if (!cells[mined].isMined) {
+        cells[mined].isMined = true;
+      } else if (cells[mined].isMined) {
+        console.log("skipped");
+        continue;
+      }
+    }
+
+    this.setState({ cells });
+    console.log("set state");
+  };
+
+  componentDidMount() {
+    this.initState();
   }
 
   render() {
@@ -20,7 +55,7 @@ class App extends React.Component {
             </h2>
           </div>
           <ControlPanel />
-          <Board />
+          <Board state={this.state} />
         </header>
       </div>
     );
--- components/Board.js
+++ components/Board.js
@@ -5,46 +5,12 @@ import "./css/Board.css";
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      cells: {},
-    };
   }
 
-  initState = () => {
-    let cells = { ...this.state.cells };
-    for (let cell = 0; cell < 72; cell++) {
-      cells[cell] = {
-        index: cell,
-        isMined: false,
-        isFlagged: false,
-        isOpen: false,
-      };
-    }
-    let mines;
-    for (mines = 0; mines < 11; mines++) {
-      let mined = Math.floor(Math.random() * Math.floor(72));
-      console.log("finding a mine");
-      console.log(mined);
-
-      if (!cells[mined].isMined) {
-        cells[mined].isMined = true;
-      } else if (cells[mined].isMined) {
-        console.log("skipped");
-        continue;
-      }
-    }
-
-    this.setState({ cells });
-    console.log("set state");
-  };
-
-  componentDidMount() {
-    this.initState();
-  }
   render() {
     return (
       <div className="board">
-        <Row state={this.state} />
+        <Row state={this.props.state} />
       </div>
     );
   }
